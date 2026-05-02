50d14adfe88a6697fd43738d521033dfba982a91
Jayasri1171 <jayasridurgaperumalla@gmail.com>
2026-05-02 12:53:50 +0530

    Working with Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Datatypes{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int studentId = 547;
+        double score = 86.9;
+        char grade = 'A';
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Score: " + score);
+        System.out.println("Grade: " + grade);
+    }
+}
