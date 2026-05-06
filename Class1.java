10af00fc96977882abb76647d09fd1b0d852c61b
Jayasri1171 <jayasridurgaperumalla@gmail.com>
2026-05-06 18:55:41 +0530

    Access variables from different files and classes

---
+++ Class1.java
@@ -0,0 +1,4 @@
+import java.util.Scanner;
+public class Class1 {
+    int x=23;
+}
---
+++ Class2.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Class2 {
+    public static void main(String[] args){
+        Class1 obj = new Class1();
+        System.out.println("The value of x in Class1 is: " + obj.x);
+    }
+}
