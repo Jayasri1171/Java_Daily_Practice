a0271f763ede4502700b0d66f63edd4fc822e7e5
Jayasri1171 <jayasridurgaperumalla@gmail.com>
2026-05-06 16:34:20 +0530

    Methods Understating

---
+++ Methodcreation.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Methodcreation {
+    static void Welcomemessage(){
+        System.out.println("Welocme to Java");
+    }
+    public static void main(String[] args){
+        Welcomemessage();
+    }
+}
